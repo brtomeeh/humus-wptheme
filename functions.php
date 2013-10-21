e09e3258a44a0348d827de929206ef48914fd595
Miguel Peixe <miguel@cardume.art.br>
2013-10-21 14:36:24 -0200

    fix wmode

--- functions.php
+++ functions.php
@@ -842,6 +842,17 @@ function humus_toggle_display_author() {
 }
 add_action('init', 'humus_toggle_display_author');
 
+function humus_add_video_wmode_transparent($html, $url, $attr) {
+	if ( strpos( $html, "<embed src=" ) !== false )	{
+		return str_replace('</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque" ', $html);
+	} elseif ( strpos ( $html, 'feature=oembed' ) !== false ) { 
+		return str_replace( 'feature=oembed', 'feature=oembed&wmode=opaque', $html );
+	} else { 
+		return $html; 
+	}
+}
+add_filter( 'embed_oembed_html', 'humus_add_video_wmode_transparent', 10, 3);
+
 
 /*
  * Connect REMIX
